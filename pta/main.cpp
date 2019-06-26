4ca5e8bd9f0511bf9409e9588f9f866e2140441f
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-26 19:03:09 +0800

    Update main.cpp

--- pta/main.cpp
+++ pta/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  pta
-//
-//  Created by s20181102929 on 2019/6/26.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+class point{
+private:
+    int x;
+    int y;
+public:
+    friend double distance(point &c,point &d);
+    friend istream & operator>>(istream & input,point & c);
+};
+istream & operator>>(istream & input,point& c){
+    input>>c.x>>c.y;
+    return input;
+}
+double distance(point &c,point &d){
+    double h=0;
+    h=pow(c.x-d.x,2)+pow(c.y-d.y,2);
+    h=sqrt(h);
+    return h;
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    point a;point b;
+    cin>>a>>b;
+    double h;
+    h=distance(a,b);
+    printf("%.2f",h);
     return 0;
 }
